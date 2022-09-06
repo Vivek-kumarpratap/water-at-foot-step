c378166429c0a244008840a86c5bf76c1b74b5d7
Vivek-kumarpratap <singharun8734@gmail.com>
2022-09-06 11:26:11 +0530

    water-at-footstep app

---
+++ app/src/main/java/com/example/vivekpratapsingh/water_at_footstep/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.vivekpratapsingh.water_at_footstep.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vivekpratapsingh.water_at_footstep.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
